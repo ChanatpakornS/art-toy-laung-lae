699a6d66a259f71102f993c7b6bd462d368b8959
Thiraput01 <Thiraputball@gmail.com>
2025-11-15 16:34:21 +0700

    fix: type order

--- src/types/arttoy.types.ts
+++ src/types/arttoy.types.ts
@@ -53,3 +53,28 @@ export interface DeleteArtToyResponse {
   success: boolean;
   message: string;
 }
+
+export interface OrderUser {
+  _id: string;
+  name: string;
+  email: string;
+}
+
+export interface OrderArtToy {
+  _id: string;
+  sku: string;
+  name: string;
+  description: string;
+  arrivalDate: string;
+  availableQuota: number;
+  posterPicture?: string;
+}
+
+export interface Order {
+  _id: string;
+  user: OrderUser | string;
+  artToy: OrderArtToy | string;
+  orderAmount: number;
+  createdAt: string;
+  updatedAt: string;
+}
