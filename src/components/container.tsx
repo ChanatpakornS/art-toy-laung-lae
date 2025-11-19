a4ec49aa49ee0ac00d7acede7361ef2e469fdb4f
csap <csirintronsopon8@gmail.com>
2025-11-19 22:29:45 +0700

    style: container

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,3 +1,3 @@
 export function Container({ children }: { children: React.ReactNode }) {
-  return <div className='max-w-3xl mx-auto px-4 pb-8'>{children}</div>;
+  return <div className='max-w-3xl mx-auto p-4'>{children}</div>;
 }
